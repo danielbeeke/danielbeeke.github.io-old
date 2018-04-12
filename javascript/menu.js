8e9d32c347cf67cc15ec1b17eb2746dda2c1fa5d
Daniel Beeke <mail@danielbeeke.nl>
2018-04-12 22:27:41 +0200

    css fix menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -3,3 +3,9 @@ let hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
   document.body.classList.toggle('has-active-menu');
 });
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode == 27 && document.body.classList.contains('has-active-menu')) {
+    document.body.classList.remove('has-active-menu')
+  }
+})
